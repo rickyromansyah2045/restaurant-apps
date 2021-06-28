b5b3913a08a82cb2413a332d9bfc3d7412207d59
rickyromansyah2045 <rickyromansyah54@gmail.com>
2021-06-28 22:34:27 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function () {
+  nav.classList.toggle('slide');
+});
